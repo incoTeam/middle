1e7873f83fa70d16160514df7a8733a2c31c16f6
Do-Do-ux <ksd4516@naver.com>
2025-02-06 10:19:10 +0900

    first commit

---
+++ backend-module/src/test/java/org/insilicogen/backendmodule/BackendModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.insilicogen.backendmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
