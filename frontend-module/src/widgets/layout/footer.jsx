8668b4e2c5194b98cfc8b8ebdf810638b0673bb8
plast <plastic333527@gmail.com>
2025-02-13 16:59:52 +0900

    2.14. 중간발표 피드백 2차 반영

--- frontend-module/src/widgets/layout/footer.jsx
+++ frontend-module/src/widgets/layout/footer.jsx
@@ -44,10 +44,10 @@ Footer.defaultProps = {
     brandName: "Creative Tim",
     brandLink: "https://www.creative-tim.com",
     routes: [
-        {name: "Creative Tim", path: "https://www.creative-tim.com"},
-        {name: "About Us", path: "https://www.creative-tim.com/presentation"},
-        {name: "Blog", path: "https://www.creative-tim.com/blog"},
-        {name: "License", path: "https://www.creative-tim.com/license"},
+        // {name: "Creative Tim", path: "https://www.creative-tim.com"},
+        // {name: "About Us", path: "https://www.creative-tim.com/presentation"},
+        // {name: "Blog", path: "https://www.creative-tim.com/blog"},
+        // {name: "License", path: "https://www.creative-tim.com/license"},
     ],
 };
 
